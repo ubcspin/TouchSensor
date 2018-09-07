5c8fe8edd7e5bac439f30130d58d440b44a6075a
Bryan Lee <41661178+bryleeant@users.noreply.github.com>
2018-09-06 19:53:15 -0700

    Added a components folder. Created Page.jsx. Moved entire Page class to Page.jsx.

---
+++ src/components/Page.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default class Page extends React.Component {
+  constructor(props) {
+		super(props);
+		this.setState({});
+		this.state = {
+			num: 5,
+		}
+  }
+
+  	myFunFn() {
+    	return ("all of the returns");
+  	}
+
+  renderDiv(n) {
+    return (<p>{n}</p>)
+  }
+
+  divFun() {
+    var arr = [];
+    for (var i = 0; i < 100; i++) {
+      arr.push(i);
+    }
+    return (
+      <div id="hi">
+        {
+            arr.map((n, i, e) => {return this.renderDiv(i)})
+        }
+      </div>
+      )
+  }
+
+  	render() {
+      	return (
+      		<div>
+      			<h1>Displaying Matrix</h1>
+          	<p>{this.myFunFn()}</p>
+          	<p>{this.state.num}</p>
+            {this.divFun()}
+      		</div>
+    	)
+  	};
+}
